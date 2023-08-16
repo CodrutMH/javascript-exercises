0653d1c369b27153eb9cd09c54fdedff22fca47b
CodrutMH <codrutmihalca@gmail.com>
2023-08-16 11:02:55 +0300

    finished first 5 exe

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitString = string.split('');
+  console.log(splitString);
+  let reverseString = splitString.reverse();
+  let joinString = reverseString.join('');
+  return joinString;
 };
 
 // Do not edit below this line
